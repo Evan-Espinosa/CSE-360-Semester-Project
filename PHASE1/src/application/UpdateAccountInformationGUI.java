504ed4274a17b176303ba93d1036179820f98e5a
Evan-Espinosa <emespin5@asu.edu>
2024-10-05 18:31:52 -0700

    Add files via upload
    GUI stub push

---
+++ PHASE1/src/application/UpdateAccountInformationGUI.java
@@ -0,0 +1,45 @@
+package application;
+
+//import java.lang.*;
+import javafx.application.Application;
+
+public class UpdateAccountInformationGUI {
+	
+	/**
+	 * Variable setup
+	 */
+	//test
+	
+	private String emailInput;
+	private String firstNameInput;
+	private String middleNameInput;
+	private String lastNameInput;
+	private String prefNameInput;
+	
+	private Label emailLabel;
+	private Label firstNameLabel;
+	private Label middleNameLabel;
+	private Label lastNameLabel;
+	private Label prefNameLabel;
+	
+	private Text emailTest;
+	private Text firstNameText;
+	private Text middleNameText;
+	private Text lastNameText;
+	private Text prefnameText;
+	
+	private Button updateButton;
+	
+	
+	/** Default constructor
+	 */
+	public void userInfo() {
+		
+	}
+	
+	public void userInfo(String email, String firstName, String middleName, String lastName, String prefName){
+		
+		// Constructor variable names are different from constructor input names to prevent variable shadowing
+		
+	}
+}
---
+++ PHASE1/src/module-info.java
@@ -0,0 +1,5 @@
+module PHASE1 {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
